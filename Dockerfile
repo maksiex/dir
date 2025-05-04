c285bea24c847b7dbde62247717c025ff0f76e92
maksie <nekroot@mail.ru>
2025-05-04 13:58:40 +0300

    fixed Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -19,5 +19,7 @@ EXPOSE 8080
 COPY wait-for-it.sh .
 RUN chmod +x wait-for-it.sh
 
+EXPOSE 8080
+
 ENTRYPOINT ["./wait-for-it.sh", "db:5432", "./dir_backv1"]
 
--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 host="$1"
 shift
@@ -6,23 +6,12 @@ cmd="$@"
 
 echo "⏳ Waiting for $host to be ready..."
 
-until nc -z $host 2>/dev/null; do
+until nc -z "$host" 2>/dev/null; do
   echo "🚧 Still waiting for $host..."
   sleep 1
 done
 
 echo "✅ $host is up. Starting application..."
-sleep 1  
-
-exec $cmd#!/usr/bin/env bash
-
-host="$1"
-shift
-cmd="$@"
-
-until nc -z ${host} 2>/dev/null; do
-  echo "Waiting for ${host}..."
-  sleep 1
-done
+sleep 1
 
 exec $cmd
