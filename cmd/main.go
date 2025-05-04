6faacdbf3868d3a3c5913cecf005652b87fb25d1
maksie <nekroot@mail.ru>
2025-05-04 19:20:39 +0300

    added test handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/maksiex/dir/internal/configs"
 	"github.com/maksiex/dir/pkg/db"
+	"io"
 	"log"
 	"net/http"
 )
@@ -12,14 +16,39 @@ func main() {
 	dbConfig := configs.LoadDBConfig()
 	db.DirDb(dbConfig)
 
-	log.Println("✅ Starting backend...")
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+	}))
+	r.Use(middleware.RequestID)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("Backend is running 🚀"))
+	r.Get("/airports", func(w http.ResponseWriter, r *http.Request) {
+		req, err := http.NewRequest("GET", "https://api.aviationapi.com/v1/airports?apt=ART&group=1", nil)
 		if err != nil {
-			log.Println(err)
+			http.Error(w, "Failed to create request", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			http.Error(w, "Failed to fetch data", http.StatusBadGateway)
+			return
+		}
+		defer resp.Body.Close()
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(resp.StatusCode)
+		_, err = io.Copy(w, resp.Body)
+		if err != nil {
+			log.Println("Failed to write response:", err)
 		}
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("✅ API listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
